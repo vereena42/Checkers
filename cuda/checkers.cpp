ea93b8a260fdf114283c2ddfd1f56c47fa74881c
Jakub Tasiemski <jakubtasiemski@MacBook-Pro-Jakub.local>
2017-01-15 22:38:12 +0100

    Cuda checkers

---
+++ cuda/checkers.cpp
@@ -0,0 +1,107 @@
+#include "cuda.h"
+#include <cstdio>
+
+struct checkers_point{
+    int board[64];
+    int how_much_children;
+    checkers_point * children = NULL;
+    checkers_point * next = NULL;
+    checkers_point * parent = NULL;
+    bool min_max;
+    int value;
+    int player;
+};
+
+
+int main(){
+    cuInit(0);
+    CUdevice cuDevice;
+    CUresult res = cuDeviceGet(&cuDevice, 0);
+    if (res != CUDA_SUCCESS){
+        printf("cannot acquire device 0\n");
+        exit(1);
+    }
+
+    CUcontext cuContext;
+    res = cuCtxCreate(&cuContext, 0, cuDevice);
+    if (res != CUDA_SUCCESS){
+        printf("cannot create context\n");
+        exit(1);
+    }
+
+    CUmodule cuModule = (CUmodule)0;
+    res = cuModuleLoad(&cuModule, "checkers.ptx");
+    if (res != CUDA_SUCCESS) {
+        printf("cannot load module: %d\n", res);
+        exit(1);
+    }
+
+    CUfunction create_tree, print_tree, nl;
+    res = cuModuleGetFunction(&create_tree, cuModule, "create_tree");
+    if (res != CUDA_SUCCESS){
+        printf("cannot acquire kernel handle\n");
+        exit(1);
+    }
+    res = cuModuleGetFunction(&print_tree, cuModule, "print_tree");
+    if (res != CUDA_SUCCESS){
+        printf("cannot acquire kernel handle\n");
+        exit(1);
+    }
+    
+    res = cuModuleGetFunction(&nl, cuModule, "new_line");
+    if (res != CUDA_SUCCESS){
+        printf("cannot acquire kernel handle\n");
+        exit(1);
+    }
+
+    
+    int how_deep = 4;
+    int max_children = 12 * 2;
+    int n = max_children;
+    for (int i = 0; i < how_deep; i++){
+        n *= max_children;
+    }
+    printf("N: %d\n", n);
+    size_t size = sizeof(checkers_point);
+    checkers_point * a = (checkers_point*) malloc(size);
+    res = cuMemHostRegister(a, size, 0);
+    if (res != CUDA_SUCCESS){
+        printf("cuMemHostRegister\n");
+        exit(1);
+    }
+    int blocks_per_grid = (n+1023)/1024;
+    int threads_per_block = 1024;
+    a->children = a->parent = a->next = NULL;
+    a->value = 1;
+    CUdeviceptr Adev;
+    res = cuMemAlloc(&Adev, size);
+    if (res != CUDA_SUCCESS){
+        printf("cuMemAlloc\n");
+        exit(1);
+    }
+    res = cuMemcpyHtoD(Adev, a, size);
+    if (res != CUDA_SUCCESS){
+        printf("cuMemcpy\n");
+        exit(1);
+    }
+    int i;
+    void* args[] = {&n, &Adev, &i};
+    for (i = 1; i < how_deep+1; i++){
+        res = cuLaunchKernel(create_tree, blocks_per_grid, 1, 1, threads_per_block, 1, 1, 0, 0, args, 0);
+        res = cuLaunchKernel(nl, blocks_per_grid, 1, 1, threads_per_block, 1, 1, 0, 0, args, 0);
+    }
+    if (res != CUDA_SUCCESS){
+        printf("cannot run kernel\n");
+        exit(1);
+    }
+    res = cuLaunchKernel(print_tree, blocks_per_grid, 1, 1, threads_per_block, 1, 1, 0, 0, args, 0);
+    if (res != CUDA_SUCCESS){
+        printf("cannot run kernel\n");
+        exit(1);
+    }
+    cuMemcpyDtoH(a, Adev, size);
+    cuMemFree(Adev);
+    printf("Result: %d\n", a->value);
+    cuCtxDestroy(cuContext);
+    return 0;
+}
---
+++ cuda/checkers.cu
@@ -0,0 +1,94 @@
+#include<cstdio>
+
+struct checkers_point{
+    int board[64];
+    int how_much_children;
+    checkers_point * children = NULL;
+    checkers_point * next = NULL;
+    checkers_point * parent = NULL;
+    bool min_max;
+    int value;
+    int player;
+};
+
+
+extern "C" {
+
+__device__
+    void ramification(checkers_point * ch2, int thid, int how_deep){
+        int pseudo_rand = thid % 7 + 2;
+        if (!(thid == 0 && how_deep == 1))
+            printf("%d | %d | %d | %d\n", thid, ch2->value, pseudo_rand, ch2->parent->value);
+        else {
+            printf("%d | %d\n", thid, pseudo_rand);
+        }
+        ch2->how_much_children = pseudo_rand;
+        ch2->children = new checkers_point;
+        ch2->children->value = ch2->value*100+1;
+        ch2->children->parent = ch2;
+        ch2 = ch2->children;
+        for (int j = 1; j < pseudo_rand; j++){
+            ch2->next = new checkers_point;
+            ch2->next->value = ch2->value+1;
+            ch2->next->parent = ch2->parent;
+            ch2 = ch2->next;
+        }
+    }
+    
+__global__
+    void create_tree(int n, checkers_point * ch, int how_deep){
+        int thid = (blockIdx.x * blockDim.x) + threadIdx.x;
+        int find_me = thid;
+        int count_group = n;
+            __syncthreads();
+        if (thid < n){
+            checkers_point * ch2 = ch;
+            for (int i = 0; i < how_deep; i++){
+                if (find_me == 0 && i + 1 == how_deep){
+                    ramification(ch2, thid, how_deep);
+                }
+                __syncthreads();
+                if (i + 1 == how_deep)
+                    break;
+                count_group = count_group/ch2->how_much_children;
+                int group = find_me/count_group;
+                if (group >= ch2->how_much_children)
+                    break;
+                find_me = find_me % count_group;
+                ch2 = ch2->children;
+                for (int k = 0; k < group; k++)
+                    ch2 = ch2->next;
+                __syncthreads();
+            }
+        }
+    }
+    
+    
+__device__
+    void print_tr(checkers_point * ch){
+        if (ch == NULL)
+            return;
+        print_tr(ch->children);
+        print_tr(ch->next);
+        printf("%d\n", ch->value);
+    }
+        
+__global__
+    void print_tree(int n, checkers_point * ch, int i){
+        int thid = (blockIdx.x * blockDim.x) + threadIdx.x;
+        if (thid == 0){
+            printf("____\n");
+            print_tr(ch);
+            printf("____\n");
+        }
+    }
+
+__global__
+    void new_line(int n, checkers_point * ch, int i){
+        int thid = (blockIdx.x * blockDim.x) + threadIdx.x;
+        if (thid == 0){
+            printf("____\n");
+        }
+    }
+
+}
